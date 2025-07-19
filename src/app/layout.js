8f70c7030a8ab1977b89ce77728bbae98943684f
Diego Ueda <dyhueda@gmail.com>
2025-07-19 17:25:25 +0900

    icon add\

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,11 +8,17 @@ const quicksand = Quicksand({
 export const metadata = {
   title: "Controle Financeiro",
   description: "Pagina para dar upload em imagens e adicionar no controle financeiro",
+  icons:{
+    icon: '/favicon.ico',
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body
         className={`${quicksand.className} bg-slate-800 text-white p-3 min-h-screen max-w-screen `} style={{isolation:"isolate"}}
       >
